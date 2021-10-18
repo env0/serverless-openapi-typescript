ce80baad647a343449af5d66ad0745946f09437b
Liran Farage <liran.farage@env0.com>
2021-10-18 10:19:59 +0300

    pr-comments - removed object schema

--- test/serverless-custom-tags/api.d.ts
+++ test/serverless-custom-tags/api.d.ts
@@ -1,37 +1,14 @@
-interface ObjectType {
-    types?: string[];
-    children?: ObjectType[];
-}
-
 export namespace ProjectApi {
 
     export namespace CreateFunc {
         export namespace Request {
-            export type Body = {
-                data: string;
-                statusCode?: number;
-                enable: boolean;
-                object?: ObjectType;
-            };
+            export type Body = void
         }
 
-        export type Response = {
-            id: string;
-            uuid: string;
-        };
+        export type Response = void
     }
 
     export namespace GetFunc {
-        export type Response = {
-            data: string;
-        };
-    }
-
-    export namespace DeleteFunc {
-        export namespace Request {
-            export type Body = {
-                id: string;
-            };
-        }
+        export type Response = void
     }
 }
