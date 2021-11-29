d113d6ed1708c3ffd1b4ba9d998f925b4cdb53f0
eranelbaz <eranelbaz97@gmail.com>
2021-11-29 17:03:03 +0200

    test files

---
+++ test/serverless-query-param-type/api.d.ts
@@ -0,0 +1,11 @@
+export namespace ProjectApi {
+    export namespace Func {
+        export namespace Request {
+            export type QueryParams = {
+                name: 'foo' | 'bar'
+            }
+        }
+
+        export type Response = void
+    }
+}
