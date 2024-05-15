a44acdc971236be8916d3ba9fbc3dfc54bb17f63
Elbaz <eranelbaz97+github@gmail.com>
2024-05-15 15:52:30 +0300

    remove pretty

--- test/serverless-full/api.d.ts
+++ test/serverless-full/api.d.ts
@@ -1,60 +1,60 @@
 interface ObjectType {
-  types?: string[];
-  children?: ObjectType[];
+    types?: string[];
+    children?: ObjectType[];
 }
 
 export namespace ProjectApi {
-  export type Bool = 'true' | 'false';
-  export type Number = number
-  export type String = string;
-  export type GenericType<T> = T[];
-  export namespace Webhooks {
-    export type OnCreateWebhook = {
-      id: string;
-      name: string;
-    }
-  }
-  export namespace CreateFunc {
-    export namespace Request {
-      export type Body = {
-        data: string;
-        statusCode?: number;
-        enable: boolean;
-        object?: ObjectType;
-      };
-    }
-
-    export type Response = {
-      id: string;
-      uuid: string;
-      generic: GenericType<{ key: string, name: number }>;
-    };
-  }
-
-  export namespace DeleteFunc {
-    export namespace Request {
-      export type Body = {
+    export type Bool = 'true' | 'false';
+    export type Number = number
+    export type String = string;
+    export type GenericType<T> = T[];
+    export namespace Webhooks {
+      export type OnCreateWebhook = {
         id: string;
-      };
+        name: string;
+      }
     }
-  }
+    export namespace CreateFunc {
+        export namespace Request {
+            export type Body = {
+                data: string;
+                statusCode?: number;
+                enable: boolean;
+                object?: ObjectType;
+            };
+        }
 
-  export namespace UpdateFunc {
-    export namespace Request {
-      export type Body = {
-        id: string;
-        data: string;
-      };
+        export type Response = {
+            id: string;
+            uuid: string;
+            generic: GenericType<{ key: string, name: number}>;
+        };
     }
 
-    export type Response = {
-      id: string;
-    };
-  }
+    export namespace DeleteFunc {
+        export namespace Request {
+            export type Body = {
+                id: string;
+            };
+        }
+    }
 
-  export namespace GetFunc {
-    export type Response = {
-      data: string;
-    };
-  }
+    export namespace UpdateFunc {
+        export namespace Request {
+            export type Body = {
+                id: string;
+                data: string;
+            };
+        }
+
+        export type Response = {
+            id: string;
+        };
+    }
+
+    export namespace GetFunc {
+        export type Response = {
+            data: string;
+        };
+    }
 }
